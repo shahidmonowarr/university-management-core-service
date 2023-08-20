541e554b6689679efeda0030a0a6b4f73f048c92
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-20 11:10:01 +0600

    fetched request for student with pagination and filters

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -2,7 +2,12 @@ import { Student } from '@prisma/client';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import {
+  studentFilterableFields,
+  studentOptionsFields,
+} from './student.constants';
 import { studentService } from './student.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -16,6 +21,31 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentFilterableFields);
+  const options = pick(req.query, studentOptionsFields);
+  const result = await studentService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Students fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const result = await studentService.getDataById(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student fetched successfully',
+    data: result,
+  });
+});
+
 export const studentController = {
   insertIntoDB,
+  getAllFromDB,
+  getDataById,
 };
--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -3,6 +3,8 @@ import { studentController } from './student.controller';
 
 const router = express.Router();
 
+router.get('/', studentController.getAllFromDB);
+router.get('/:id', studentController.getDataById);
 router.post('/', studentController.insertIntoDB);
 
 export const studentRoutes = router;
