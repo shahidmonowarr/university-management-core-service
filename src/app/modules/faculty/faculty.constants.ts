0a8ad0cdc825418c9d98dfa3e566f2ff6a7d03cf
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-20 12:36:37 +0600

    fetched request for faculty with pagination and filters

---
+++ src/app/modules/faculty/faculty.constants.ts
@@ -0,0 +1,38 @@
+export const facultyFilterableFields: string[] = [
+  'searchTerm',
+  'facultyId',
+  'email',
+  'contactNo',
+  'gender',
+  'bloodGroup',
+  'gender',
+  'designation',
+  'academicFacultyId',
+  'academicDepartmentId',
+];
+
+export const facultySearchableFields: string[] = [
+  'firstName',
+  'lastName',
+  'middleName',
+  'email',
+  'contactNo',
+  'facultyId',
+  'designation',
+];
+
+export const facultyRelationalFields: string[] = [
+  'academicFacultyId',
+  'academicDepartmentId',
+];
+export const facultyRelationalFieldsMapper: { [key: string]: string } = {
+  academicFacultyId: 'academicFaculty',
+  academicDepartmentId: 'academicDepartment',
+};
+
+export const facultyOptionsFields: string[] = [
+  'limit',
+  'page',
+  'sortBy',
+  'sortOrder',
+];
--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -3,6 +3,8 @@ import { facultyController } from './faculty.controller';
 
 const router = express.Router();
 
+router.get('/', facultyController.getAllFromDB);
+router.get('/:id', facultyController.getDataById);
 router.post('/', facultyController.insertIntoDB);
 
 export const facultyRoutes = router;
