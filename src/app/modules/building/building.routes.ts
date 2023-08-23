fa5f63479e02889cd60966b6cc6db4aa38e4163f
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-23 21:33:13 +0600

    validation and permission for create building

--- src/app/modules/building/building.routes.ts
+++ src/app/modules/building/building.routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { buildingController } from './building.controller';
+import { buildingValidation } from './building.validation';
 
 const router = express.Router();
 
 router.get('/', buildingController.getAllFromDB);
-router.post('/', buildingController.insertIntoDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(buildingValidation.create),
+  buildingController.insertIntoDB
+);
 
 export const buildingRoutes = router;
