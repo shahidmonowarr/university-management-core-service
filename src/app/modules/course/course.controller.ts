7899a8f7f164ecf671ae08ea468247a708ee55d2
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-24 14:07:35 +0600

    course get-delete & role added"

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { courseFilterableFields } from './course.constants';
 import { courseService } from './course.service';
 
 const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
@@ -17,6 +19,44 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, courseFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await courseService.getAllFromDB(filters, options);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Courses fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await courseService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course fetched successfully',
+    data: result,
+  });
+});
+
+const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await courseService.deleteByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Course deleted successfully',
+    data: result,
+  });
+});
+
 export const courseController = {
   insertIntoDB,
+  getAllFromDB,
+  getByIdFromDB,
+  deleteByIdFromDB,
 };
--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { courseController } from './course.controller';
+import { courseValidation } from './course.validation';
 
 const router = express.Router();
 
-router.post('/', courseController.insertIntoDB);
+router.get('/', courseController.getAllFromDB);
+router.get('/:id', courseController.getByIdFromDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(courseValidation.create),
+  courseController.insertIntoDB
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  courseController.deleteByIdFromDB
+);
 
 export const courseRoutes = router;
