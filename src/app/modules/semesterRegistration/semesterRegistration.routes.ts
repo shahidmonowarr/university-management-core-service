55edbd990cc54c8757b4a784e588d572d6650226
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-28 18:21:26 +0600

    validation and permission for registration

--- src/app/modules/semesterRegistration/semesterRegistration.routes.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.routes.ts
@@ -1,12 +1,30 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
 import { semesterRegistrationController } from './semesterRegistration.controller';
+import { semesterRegistrationValidation } from './semesterRegistration.validation';
 
 const router = express.Router();
 
 router.get('/', semesterRegistrationController.getAllFromDB);
 router.get('/:id', semesterRegistrationController.getOneFromDB);
-router.post('/', semesterRegistrationController.insertIntoDB);
-router.patch('/:id', semesterRegistrationController.updateOneInDB);
-router.delete('/:id', semesterRegistrationController.deleteOneFromDB);
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(semesterRegistrationValidation.create),
+  semesterRegistrationController.insertIntoDB
+);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(semesterRegistrationValidation.update),
+  semesterRegistrationController.updateOneInDB
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  semesterRegistrationController.deleteOneFromDB
+);
 
 export const semesterRegistrationRoutes = router;
