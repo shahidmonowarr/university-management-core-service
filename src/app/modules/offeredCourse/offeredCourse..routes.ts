7b119fa8e8140c6446006a6a3b8901c1057385f2
shahidmonowarr <shahidmonowar21@gmail.com>
2023-08-30 22:24:21 +0600

    offered courses get, update, delete, validation

--- src/app/modules/offeredCourse/offeredCourse..routes.ts
+++ src/app/modules/offeredCourse/offeredCourse..routes.ts
@@ -7,11 +7,19 @@ import { offeredCourseValidation } from './offeredCourse.validation';
 
 const router = express.Router();
 
+router.get('/', offeredCourseController.getAllFromDB);
+router.get('/:id', offeredCourseController.getByIdFromDB);
 router.post(
   '/',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(offeredCourseValidation.create),
   offeredCourseController.insertIntoDB
+);
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(offeredCourseValidation.update),
+  offeredCourseController.updateOneInDB
 );
 
 export const offeredCourseRoutes = router;
