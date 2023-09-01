b39eb8780ab62d55abbfe6087798e8737f7e7de2
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-01 11:28:04 +0600

    offered course class schedule get with filter

---
+++ src/app/modules/offeredCourseClassSchedule/offeredCourseClassSchedule.constants.ts
@@ -0,0 +1,26 @@
+export const offeredCourseClassScheduleSearchableFields = ['dayOfWeek'];
+
+export const offeredCourseClassScheduleRelationalFields = [
+  'offeredCourseSectionId',
+  'semesterRegistrationId',
+  'roomId',
+  'facultyId',
+];
+
+export const offeredCourseClassScheduleRelationalFieldsMapper: {
+  [key: string]: string;
+} = {
+  offeredCourseSectionId: 'offeredCourseSection',
+  semesterRegistrationId: 'semesterRegistration',
+  roomId: 'room',
+  facultyId: 'faculty',
+};
+
+export const offeredCourseClassScheduleFilterableFields = [
+  'searchTerm',
+  'dayOfWeek',
+  'offeredCourseSectionId',
+  'semesterRegistrationId',
+  'roomId',
+  'facultyId',
+];
