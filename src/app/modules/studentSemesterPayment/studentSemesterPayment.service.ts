ade1c368140aac843e278403cab8e2b9159db8b8
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-10 16:31:26 +0600

    student semester payment filter

--- src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
+++ src/app/modules/studentSemesterPayment/studentSemesterPayment.service.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient, StudentSemesterPayment } from '@prisma/client';
 import {
   DefaultArgs,
   PrismaClientOptions,
 } from '@prisma/client/runtime/library';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../shared/prisma';
+import {
+  studentSemesterPaymentRelationalFields,
+  studentSemesterPaymentRelationalFieldsMapper,
+  studentSemesterPaymentSearchableFields,
+} from './studentSemesterPayment.constants';
+import { IStudentSemesterPaymentFilterRequest } from './studentSemesterPayment.interface';
 
 const createSemesterPayment = async (
   prismaClient: Omit<
@@ -42,6 +52,79 @@ const createSemesterPayment = async (
   }
 };
 
+const getAllFromDB = async (
+  filters: IStudentSemesterPaymentFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<StudentSemesterPayment[]>> => {
+  const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: studentSemesterPaymentSearchableFields.map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map(key => {
+        if (studentSemesterPaymentRelationalFields.includes(key)) {
+          return {
+            [studentSemesterPaymentRelationalFieldsMapper[key]]: {
+              id: (filterData as any)[key],
+            },
+          };
+        } else {
+          return {
+            [key]: {
+              equals: (filterData as any)[key],
+            },
+          };
+        }
+      }),
+    });
+  }
+
+  const whereConditions: Prisma.StudentSemesterPaymentWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.studentSemesterPayment.findMany({
+    include: {
+      academicSemester: true,
+      student: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: 'desc',
+          },
+  });
+  const total = await prisma.studentSemesterPayment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const studentSemesterPaymentService = {
   createSemesterPayment,
+  getAllFromDB,
 };
