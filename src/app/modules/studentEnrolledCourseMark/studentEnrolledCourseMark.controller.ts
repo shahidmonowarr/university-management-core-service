e37ab2053d1b6f60840ce82104a814fffdd3c3ab
shahidmonowarr <shahidmonowar21@gmail.com>
2023-09-16 23:18:00 +0600

    Retrieviing Student Course Marks

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
@@ -48,8 +48,28 @@ const updateFinalMarks = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getMyCourseMarks = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseMarkFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const user = (req as any).user;
+
+  const result = await studentEnrolledCourseMarkService.getMyCourseMarks(
+    filters,
+    options,
+    user
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student course marks fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
 export const studentEnrolledCourseMarkController = {
   getAllFromDB,
   updateStudentMarks,
   updateFinalMarks,
+  getMyCourseMarks,
 };
